25e025db1dd9252e18e36d7a6e77fe199d18dba7
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-04-20 17:46:04 -0400

    Get single product from server on backend

--- server/api/products.js
+++ server/api/products.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Product } = require('../db');
+const { Product } = require('../db/models');
 module.exports = router;
 
 // GET /api/products
@@ -8,7 +8,18 @@ router.get('/', async (req, res, next) => {
     const allProducts = await Product.findAll();
     res.send(allProducts);
   } catch (err) {
-    console.error('Unable to get all products...');
+    console.error('🥎 Unable to get all products...');
+    next(err);
+  }
+});
+
+// GET /api/products/:productId
+router.get('/:productId', async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.productId);
+    res.send(product);
+  } catch (err) {
+    console.error('🏓 Unable to get single product...');
     next(err);
   }
 });
