d26b99d21ad0a9a1000408d9584741bcd5f74ef1
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-04-20 16:45:14 -0400

    Link to single product from all products page

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
-import { connect } from 'react-redux'
-import { fetchProducts } from '../store/products'
+import { connect } from 'react-redux';
+import { fetchProducts } from '../store/products';
+import SingleProduct from './SingleProduct';
+import { Link } from 'react-router-dom';
 
 //this component would show all products
 {
@@ -13,26 +15,40 @@ import { fetchProducts } from '../store/products'
 //Drop down menu for above to filter by categories
 //Separate filter section to filter by other filters discussed
 
+//TODO: refactor mapState and mapDispatch to hooks
+
 const AllProducts = ({ products, loadProducts }) => {
-  useEffect(() => { loadProducts() }, [])
-  if (products.length === 0) return <div>Loading ...</div>
-  return <div>{products.map((product) => {
-    return <div key={product.id}>
-      <img src= {product.image} alt={product.name} />
-      <div>{`${product.name} $${product.price}`}</div>
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
+  if (products.length === 0) return <div>Loading ...</div>;
+
+  return (
+    <div>
+      {products.map((product) => {
+        return (
+          <div key={product.id}>
+            <Link to={`/product/${product.id}`} component={SingleProduct}>
+              <img src={product.image} alt={product.name} />
+              <div>{`${product.name} $${product.price}`}</div>
+            </Link>
+          </div>
+        );
+      })}
     </div>
-  })}</div>
+  );
 };
 
 const mapState = (state) => {
   return {
-    products: state.products
-  }
-}
+    products: state.products,
+  };
+};
 
 const mapDispatch = (dispatch) => {
   return {
-    loadProducts: () => dispatch(fetchProducts())
-  }
-}
+    loadProducts: () => dispatch(fetchProducts()),
+  };
+};
 export default connect(mapState, mapDispatch)(AllProducts);
