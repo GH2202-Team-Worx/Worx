91f156ee1af4378d533a31fd996dac1f6af56738
Angel Robish <robishangel@gmail.com>
2022-04-21 13:11:37 -0400

    fixing SingleProduct page, adding cart button, changing AllProducts thunk to getProducts

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -1,5 +1,3 @@
-//TODO: add to cart button
-
 import React, { useEffect} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchProduct } from '../store/singleProduct';
@@ -7,15 +5,26 @@ import { fetchProduct } from '../store/singleProduct';
 const SingleProduct = (props) => {
   const dispatch = useDispatch()
   const product = useSelector((state) => {
-    return state.product
+    return state.singleProduct
   })
+
   const productId = props.match.params.productId;
+
   useEffect(() => {
     dispatch(fetchProduct(productId));
   }, []);
 
-  return <React.Fragment>{product.name}</React.Fragment>;
+  if (!product) {
+    return <div>Loading...</div>
+  }
+  return (<React.Fragment>
+    <img src={product.image} alt={product.name} />
+    <div>{product.name}</div>
+    <div>{product.price}</div>
+    <div>{product.material} {product.epoxyColor}</div>
+    <div>{product.description}</div>
+    <button type= 'submit'>Add to Cart</button>
+  </React.Fragment>);
 };
-
 
 export default SingleProduct;
