ae818237707307923798df0c1551ac10367735f3
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-04-22 17:15:54 -0400

    Research - instructions on Contact for sending msg

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -5,3 +5,13 @@ const Contact = () => {
 };
 
 export default Contact;
+
+//make react component on the frontend -
+//  instructions on sending email. mention order # if referring to an order. should hear back to 2-3 days (??)
+//  react form with contact information(name, email, message)
+//  set local state for name email and message.
+//  when sending, submit button can change to sending...
+//  **put front end checks in to make sure none of the boxes are empty when sending and email is a valid email
+
+//send email on backend -
+//  after email is sent, the frontend can change to either a success component ("Thank you for contacting us. You should hear back within x amt of days") or a failed to send component
