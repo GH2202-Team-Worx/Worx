833aea74d1c08fc2bdfe04684ad0475648a48e7d
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-04-22 21:19:59 -0400

    Basic contact form

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -1,10 +1,43 @@
 import React from 'react';
 
 const Contact = () => {
-  return <h1>I am a contact page</h1>;
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>We'd love to hear from you!</p>
+      <p>
+        Please feel free to contact us via phone, email, or the contact form
+        below. If contacting us about an order, kindly include your order
+        number. We will get back to you within 3-5 days via email. If the issue
+        is more urgent, you will receive a quicker reply via phone.
+      </p>
+      <p>Email: bellscustomworx@gmail.com</p>
+      <p>Phone number: (843) 534-8946</p>
+
+      <form>
+        <label>
+          * Email:
+          <input type="text" name="email" />
+        </label>
+        <label>
+          * Subject:
+          <input type="text" name="email" />
+        </label>
+        <label>
+          * Message:
+          <input type="text" name="email" />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
+
+//TODO:
+//1. make stars red in CSS
+//2. make h1 smaller
 
 //make react component on the frontend -
 //  instructions on sending email. mention order # if referring to an order. should hear back to 2-3 days (??)
