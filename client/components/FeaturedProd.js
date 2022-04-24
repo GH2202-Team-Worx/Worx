7ce19e7fb9a0722def89470d49c5509e23fe3c18
Brooke Stenglein <b.morsekarzen@gmail.com>
2022-04-24 10:20:39 -0500

    fixed featured products intruding into navbar and added styling to featured products to reflect wireframe

--- client/components/FeaturedProd.js
+++ client/components/FeaturedProd.js
@@ -1,31 +1,36 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 // import { connect } from 'react-redux';
-import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { getFeatured } from '../store/products';
-import './styles/featuredProds.css';
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { getFeatured } from "../store/products";
+import "./styles/featuredProds.css";
 
 const FeaturedProd = () => {
   const dispatch = useDispatch();
-  const featuredProds = useSelector(state => state.products)
+  const featuredProds = useSelector((state) => state.products);
   useEffect(() => {
     dispatch(getFeatured());
   }, []);
 
   return (
     <>
-      <h2>Featured Products:</h2>
       <div id="featuredProds">
-        {featuredProds.map((prod) => {
-          return (
-            <div className="featuredProd" key={prod.id}>
-              <Link to={`/products/${prod.id}`}>
-                <img src={prod.image} alt={prod.name} />
-                <h4>{`${prod.name}`}</h4>
-              </Link>
-            </div>
-          );
-        })}
+        <h2 className="featured-products-title">Featured Products</h2>
+        <div className="featured-prods-container">
+          {featuredProds.map((prod) => {
+            return (
+              <div className="featuredProd" key={prod.id}>
+                <Link
+                  className="featured-prods-description"
+                  to={`/products/${prod.id}`}
+                >
+                  <img src={prod.image} alt={prod.name} />
+                  <h4>{`${prod.name}`}</h4>
+                </Link>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </>
   );
