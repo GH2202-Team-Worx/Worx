4595548943a0d43ff59c9e46f023cae2906005d3
Brooke Stenglein <b.morsekarzen@gmail.com>
2022-06-01 10:46:23 -0500

    changed booleans to yes/no, updated styling for client cards

--- client/components/Admin/AllUsers.js
+++ client/components/Admin/AllUsers.js
@@ -1,26 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import AdminDashboard from "./AdminDash";
-import { fetchUpdatedUser, fetchUsers, DeleteUser } from "../../store/users";
+import { fetchUsers, DeleteUser } from "../../store/users";
 import { updateUser } from "../../store/newUser";
 import "../styles/AllUsers.css";
 
 const AllUsers = (props) => {
   const dispatch = useDispatch();
   const allUsers = useSelector((state) => {
-    // console.log(state.usersReducer);
     return state.usersReducer;
+  });
+  const user = useSelector((state) => {
+    return state.createUserReducer;
   });
 
   useEffect(() => {
     if (allUsers) {
       dispatch(fetchUsers());
     }
-  }, []);
-
-  const [admin, setAdmin] = useState(false);
-  const [userId, setUserId] = useState("");
+  }, [user]);
 
   const handleSubmit = (event, userId) => {
     event.preventDefault();
@@ -38,36 +37,23 @@ const AllUsers = (props) => {
     listOfUsers = allUsers.map((user) => {
       return (
         <div className="user-card" key={user.id}>
-          <h4>
-            Name: {user.firstName} {user.lastName}
-          </h4>
-          <div>Email: {user.email}</div>
-          <div>Admin: {user.isAdmin}</div>
-
+          <div>
+            <strong>Email:</strong> {user.email}
+          </div>
+          <div>
+            <strong>Admin:</strong> {user.isAdmin === true ? "Yes" : "No"}
+          </div>
+          <div>Give admin status?</div>
           <select
-            className="category-dropdown"
+            className="admin-status-dropdown"
             name="status"
-            // placeholder={user.isAdmin}
             value={user.isAdmin}
             onChange={(evt) => handleSubmit(evt, user.id)}
           >
             <option value="">Select status</option>
-            <option value="true">True</option>
-            <option value="false">False</option>
+            <option value="true">Yes</option>
+            <option value="false">No</option>
           </select>
-
-          {/* <form onSubmit={handleSubmit}>
-            <div>Give Admin status?</div>
-            <input
-              value={user.isAdmin}
-              type="text"
-              onChange={(e) => {
-                setAdmin(e.target.value);
-              }}
-            />
-            <button type="submit">Change Status</button>
-          </form> */}
-
           <button
             type="button"
             onClick={(event) => deleteHandler(event, user.id)}
