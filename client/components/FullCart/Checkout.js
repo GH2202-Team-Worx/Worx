bf63e52a64db29497ffd35d69d1057c1ac3c111a
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-11-28 13:01:34 -0500

    Remove comment

--- client/components/FullCart/Checkout.js
+++ client/components/FullCart/Checkout.js
@@ -1,14 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { sendOrder } from "../../store/cart";
-import ShippingInformation from "./ShippingInformation";
-// import BillingInformation from './BillingInformation';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { sendOrder } from '../../store/cart';
+import ShippingInformation from './ShippingInformation';
 import {
   PaymentElement,
   useStripe,
   useElements,
-} from "@stripe/react-stripe-js";
-import "../styles/Checkout.css";
+} from '@stripe/react-stripe-js';
+import '../styles/Checkout.css';
 
 //notes from server/api/orders
 //if user is guest, front end should save the cart locally and only send to back end route "api/order/" w/ status "Processing" once order is placed.
@@ -19,11 +18,11 @@ const Checkout = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [shipping, setShipping] = useState({
     phone: 0,
-    shipName: "",
-    streetOne: "",
-    streetTwo: "",
-    city: "",
-    state: "",
+    shipName: '',
+    streetOne: '',
+    streetTwo: '',
+    city: '',
+    state: '',
     zip: 0,
   });
   // const [billing, setBilling] = useState({
@@ -61,7 +60,7 @@ const Checkout = () => {
     }
 
     const clientSecret = new URLSearchParams(window.location.search).get(
-      "payment_intent_client_secret"
+      'payment_intent_client_secret'
     );
 
     if (!clientSecret) {
@@ -70,22 +69,22 @@ const Checkout = () => {
 
     stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
       switch (paymentIntent.status) {
-        case "succeeded":
-          setMessage("Payment succeeded!");
-          console.log("SUCCESS 💅🏼");
+        case 'succeeded':
+          setMessage('Payment succeeded!');
+          console.log('SUCCESS 💅🏼');
           // dispatch(sendOrder(cartItems, cartTotal));
           break;
-        case "processing":
-          setMessage("Your payment is processing.");
-          console.log("PROCESSING 🦺");
+        case 'processing':
+          setMessage('Your payment is processing.');
+          console.log('PROCESSING 🦺');
           break;
-        case "requires_payment_method":
-          setMessage("Your payment was not successful, please try again.");
-          console.log("TRY AGAIN 🦄");
+        case 'requires_payment_method':
+          setMessage('Your payment was not successful, please try again.');
+          console.log('TRY AGAIN 🦄');
           break;
         default:
-          setMessage("Something went wrong.");
-          console.log("FAILURE ⛑");
+          setMessage('Something went wrong.');
+          console.log('FAILURE ⛑');
 
           break;
       }
@@ -107,7 +106,7 @@ const Checkout = () => {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:8080/order/confirmation",
+        return_url: 'http://localhost:8080/order/confirmation',
       },
     });
 
@@ -116,10 +115,10 @@ const Checkout = () => {
     // your `return_url`. For some payment methods like iDEAL, your customer will
     // be redirected to an intermediate site first to authorize the payment, then
     // redirected to the `return_url`.
-    if (error.type === "card_error" || error.type === "validation_error") {
+    if (error.type === 'card_error' || error.type === 'validation_error') {
       setMessage(error.message);
     } else {
-      setMessage("An unexpected error occured.");
+      setMessage('An unexpected error occured.');
     }
 
     setIsLoading(false);
@@ -147,7 +146,7 @@ const Checkout = () => {
                 {isLoading ? (
                   <div className="spinner" id="spinner"></div>
                 ) : (
-                  "Submit payment"
+                  'Submit payment'
                 )}
               </span>
             </button>
@@ -171,7 +170,7 @@ const Checkout = () => {
                   {/* <li>{item.description}</li> */}
                   <li>Price: {`$${item.price}`}</li>
                   <li>{`Customization: ${
-                    item.customization ? item.customization : "None"
+                    item.customization ? item.customization : 'None'
                   }`}</li>
                   {item.isGift ? <li>"This item is a gift</li> : null}
                 </ul>
